2e6fe7e119bb891ff21bc53d5d0ae6b39214c7db
marccla <marcclarsson@gmail.com>
2020-03-03 14:22:53 +0100

    first files created

---
+++ products.php
@@ -0,0 +1,20 @@
+<? 
+
+    class Product {
+        // Properties
+        public $name;
+        public $price;
+        public $priceVat
+        // Method
+        public function setname($price) {
+            $this->price = $price;
+        }
+
+    }
+
+
+
+
+
+
+?>
