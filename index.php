847db79640734009f1b679874c2922399955466a
marccla <marcclarsson@gmail.com>
2020-03-03 17:49:39 +0100

    small fixes again

--- index.php
+++ index.php
@@ -17,14 +17,20 @@ include  'products.php';
 <body>
 <?
 
-$product1 = new ProductVat(25, 'derrr');
-$product2 = new ProductVat(33, 'brypp');
+$product1 = new ProductVat(25, 'Nike Air');
+$product2 = new ProductVat(33, 'Steak');
 echo $product1->vat;
 
 
 ?>
 <? var_dump($product1); ?> <br>
 <? var_dump($product2); ?>
+    <h2>Product:<? echo $product1->name ?></h2>
+    
+    <p>Price:<? echo $product1->priceWithVat ?> (VAT:<? echo $product1->vat * 100; ?>%) </p>
+    <h2>Product:<? echo $product2->name ?></h2>
+    
+    <p>Price:<? echo $product2->priceWithFoodVat ?> (VAT:<? echo $product2->foodVat * 100; ?>%) </p>
     
 </body>
 </html>
